ff2b67c690b5a8205e34efa8e4da7aa1e8af276c
xieliming2017 <xieliming2006@hotmail.com>
2017-12-30 01:38:10 -0500

    blocipedia4 completed, also added random data module

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-
-  root 'welcome#index'
-  get 'about' => 'welcome#about'
-
+  resources :wikis
 
   devise_for :users
+
+  root 'welcome#index'
+
+  get 'about' => 'welcome#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
