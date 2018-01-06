8e23142c139fefd20f9a2ebac0c1314d3ec0cd3b
xieliming2017 <xieliming2006@hotmail.com>
2018-01-06 02:19:47 -0500

    stripe installed and tested

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,30 @@
+class ChargesController < ApplicationController
+  def create
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: 1000,
+      description: "BigMoney Membership - #{current_user.email}",
+      currency: 'usd'
+    )
+
+    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+    redirect_to root_path
+
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_charge_path
+  end
+
+  def new
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "BigMoney Membership - #{current_user.email}",
+      amount: "1000_00"
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # resources :users
 
   devise_for :users
+  resources :charges, only: [:new, :create]
+
 
   root 'welcome#index'
 
