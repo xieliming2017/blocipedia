ad8d6cd0274744a9028dcaa27652d4079b02aab7
xieliming2017 <xieliming2006@hotmail.com>
2018-01-06 00:56:20 -0500

    blocipedia 6 data seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+  10.times do
+    User.create!(
+      email: RandomData.random_email,
+      password: "password",
+      role: "standard"
+      )
+  end
+
+  users = User.all
+
+  50.times do
+    Wiki.create!(
+      title: RandomData.random_sentence,
+      body: RandomData.random_paragraph,
+      private: false,
+      user: users.sample
+    )
+  end
+
+  wikis = Wiki.all
+
+  puts "Seeding finished"
+  puts "#{users.count} users created"
+  puts "#{wikis.count} wikis created"
